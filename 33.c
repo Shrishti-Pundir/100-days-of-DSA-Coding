815d4472c8cbc6d6c4f99a2ce2f5d166f5ee231f
Shrishti Pundir <shrishtipundir9758@gmail.com>
2026-03-06 08:12:43 +0530

    Day 33 Ques 1
    Day 33 Ques 1

---
+++ 33.c
@@ -0,0 +1,116 @@
+/*Problem: Convert an infix expression to postfix notation using stack.
+
+Input:
+- Single line: infix expression (operands are single characters)
+
+Output:
+- Print the postfix expression
+
+Example:
+Input:
+A+B*C
+
+Output:
+ABC*+
+
+Explanation:
+Operator precedence: * > +
+Use stack to handle operator precedence and associativity*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_SIZE 100
+
+char stack[MAX_SIZE];
+int top = -1;
+
+void push(char c) {
+    if (top >= MAX_SIZE - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = c;
+}
+
+char pop() {
+    if (top == -1) {
+        return '\0';
+    }
+    return stack[top--];
+}
+
+char peek() {
+    if (top == -1) {
+        return '\0';
+    }
+    return stack[top];
+}
+
+int precedence(char op) {
+    switch(op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+void infixToPostfix(char* infix, char* postfix) {
+    int i, j = 0;
+    
+    for (i = 0; infix[i] != '\0'; i++) {
+        char c = infix[i];
+        
+        if (isalnum(c)) {
+            postfix[j++] = c;
+        }
+        else if (c == '(') {
+            push(c);
+        }
+        else if (c == ')') {
+            while (peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // Remove '('
+        }
+        else if (isOperator(c)) {
+            while (top != -1 && precedence(peek()) >= precedence(c) && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            push(c);
+        }
+    }
+    
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX_SIZE];
+    char postfix[MAX_SIZE];
+    
+    scanf("%s", infix);
+    
+    infixToPostfix(infix, postfix);
+    
+    printf("%s\n", postfix);
+    
+    return 0;
+}
