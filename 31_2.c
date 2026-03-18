62676aca0bb8539fbb19b7d33b7404e2db55d461
Shrishti Pundir <shrishtipundir9758@gmail.com>
2026-03-18 23:39:56 +0530

    Day 31 Leetcode
    Day 31 Leetcode

---
+++ 31_2.c
@@ -0,0 +1,67 @@
+/*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false*/
+
+
+
+bool isValid(char* s) {
+    char stack[10000];
+    int top = -1;
+    
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top] = c;
+        } else {
+            if (top == -1) {
+                return false;
+            }
+            
+            char topChar = stack[top--];
+            
+            if ((c == ')' && topChar != '(') ||
+                (c == '}' && topChar != '{') ||
+                (c == ']' && topChar != '[')) {
+                return false;
+            }
+        }
+    }
+    
+    return top == -1;
+}
