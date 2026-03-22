1b1b8b662b2cf9c4cc825bda73178ccedec277d0
Shrishti Pundir <shrishtipundir9758@gmail.com>
2026-03-22 19:10:30 +0530

    Day 49 Ques 1
    Day 49 Ques 1

---
+++ 49.c
@@ -0,0 +1,68 @@
+/*Problem: BST Insert
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+    
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    struct Node* root = NULL;
+    int n, value;
+    
+    printf("Enter number of elements to insert: ");
+    scanf("%d", &n);
+    
+    printf("Enter %d values:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+    
+    printf("Inorder traversal of BST: ");
+    inorder(root);
+    printf("\n");
+    
+    return 0;
+}
